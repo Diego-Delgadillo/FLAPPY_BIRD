20362025a93123286276e4c0ff228ac8173f2917
Diego-Delgadillo <drdg2005@gmail.com>
2024-04-05 13:42:14 -0600

    Agregar archivos de proyecto.

---
+++ FLAPPY_BIRD/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FLAPPY_BIRD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pipeTop = new PictureBox();
+            flappyByrd = new PictureBox();
+            pipeBottom = new PictureBox();
+            ground = new PictureBox();
+            scoreText = new Label();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pipeTop).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)flappyByrd).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pipeBottom).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ground).BeginInit();
+            SuspendLayout();
+            // 
+            // pipeTop
+            // 
+            pipeTop.Image = Properties.Resources.pipedown;
+            pipeTop.Location = new Point(471, -11);
+            pipeTop.Name = "pipeTop";
+            pipeTop.Size = new Size(100, 250);
+            pipeTop.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeTop.TabIndex = 0;
+            pipeTop.TabStop = false;
+            // 
+            // flappyByrd
+            // 
+            flappyByrd.AccessibleName = "";
+            flappyByrd.Image = Properties.Resources.bird;
+            flappyByrd.Location = new Point(62, 175);
+            flappyByrd.Name = "flappyByrd";
+            flappyByrd.Size = new Size(82, 64);
+            flappyByrd.TabIndex = 1;
+            flappyByrd.TabStop = false;
+            flappyByrd.Click += pictureBox2_Click;
+            // 
+            // pipeBottom
+            // 
+            pipeBottom.Image = Properties.Resources.pipe;
+            pipeBottom.Location = new Point(355, 369);
+            pipeBottom.Name = "pipeBottom";
+            pipeBottom.Size = new Size(100, 274);
+            pipeBottom.SizeMode = PictureBoxSizeMode.StretchImage;
+            pipeBottom.TabIndex = 2;
+            pipeBottom.TabStop = false;
+            // 
+            // ground
+            // 
+            ground.Image = Properties.Resources.ground;
+            ground.Location = new Point(-9, 649);
+            ground.Name = "ground";
+            ground.Size = new Size(681, 89);
+            ground.SizeMode = PictureBoxSizeMode.StretchImage;
+            ground.TabIndex = 3;
+            ground.TabStop = false;
+            // 
+            // scoreText
+            // 
+            scoreText.AutoSize = true;
+            scoreText.Font = new Font("Verdana", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            scoreText.Location = new Point(21, 24);
+            scoreText.Name = "scoreText";
+            scoreText.Size = new Size(123, 29);
+            scoreText.TabIndex = 4;
+            scoreText.Text = "Score: 0";
+            scoreText.Click += label1_Click;
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 20;
+            gameTimer.Tick += gameTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Aqua;
+            ClientSize = new Size(597, 711);
+            Controls.Add(scoreText);
+            Controls.Add(ground);
+            Controls.Add(pipeBottom);
+            Controls.Add(flappyByrd);
+            Controls.Add(pipeTop);
+            Name = "Form1";
+            Text = "Flappy Byrd";
+            KeyDown += gameKeyIsDown;
+            KeyUp += gameKeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)pipeTop).EndInit();
+            ((System.ComponentModel.ISupportInitialize)flappyByrd).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pipeBottom).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ground).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pipeTop;
+        private PictureBox flappyByrd;
+        private PictureBox pipeBottom;
+        private PictureBox ground;
+        private Label scoreText;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
---
+++ FLAPPY_BIRD/Form1.cs
@@ -0,0 +1,35 @@
+namespace FLAPPY_BIRD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameKeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void gameKeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
